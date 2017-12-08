9c4ea7944d0b4e8c56a3cea76e961288f45224da
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2017-12-08 21:13:47 +0100

    Creo los modelos Category y Movie y sus migraciones ( y ). Con un visor de sqlite compruebo que se han creado esas dos tablas en la b.d.

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)  # optional
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=150)
+    summary = models.TextField()  # sin límite de longitud
+    director_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField()  # ImageField()
+    rating = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)  # saves the date when the object is created
+    modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
