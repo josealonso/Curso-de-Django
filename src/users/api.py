76182889324dd5464186f808fa166467cf78990c
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2017-12-15 17:40:06 +0100

    Implemento la ruta de creacion de un usuario

--- src/users/api.py
+++ src/users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -19,9 +20,19 @@ class HelloWorld(APIView):
 
 class UsersListAPI(APIView):
 
+
     def get(self, request):
         users = User.objects.all()  # users es un objeto q hay q convertir al formato de salida
         serializer = UserSerializer(users, many=True)  # muchos usuarios
         #  El serializador obtiene un diccionario por cada usuario de la lista
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()  #  Llama a create o a update
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
